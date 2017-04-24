57373cadc554bb2a18101149436ff9924e7abe9b
Adam Marc Williams <marc@rhythm.digital>
2017-04-24 10:46:56 +0100

    Removes redundent components and cleans up HTML structure

--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
 import './components/aframe-animation-component';
-import './components/scale-on-mouseenter';
-import './components/rotate-on-click';
 import './components/move-along-path';
 import './components/light-along-path';
 import './components/mesh-lambert-material';
